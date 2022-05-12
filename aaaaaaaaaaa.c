4978fcf57b8ed41ebe2535b56a23b9d4e76f8cf4
UsaidAhmed12 <96737343+UsaidAhmed12@users.noreply.github.com>
2022-05-13 00:07:02 +0500

    Add files via upload

---
+++ aaaaaaaaaaa.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void sum()
+{
+	int a,b,sum=0;
+	printf("enter two numbers :");
+	
+	scanf("%d%d",&a,&b);
+	sum = a+b;
+	printf("sum = %d \n",sum);
+	
+}
+
+void main()
+{
+	sum();
+	sum();
+	sum();
+	sum();
+	sum();
+}
